5c2e4ebf2b26a94f15759f65a0f29c0f78fd7a64
Source-Kod <Source-Kod@protonmail.com>
2023-12-09 17:49:45 -0600

    add: initial project configuration

---
+++ flake.nix
@@ -0,0 +1,27 @@
+# flake.nix
+
+{
+  description = "For CV project";
+
+  inputs =
+    {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              nodejs
+              typescript
+            ];
+          };
+    };
+}
+
